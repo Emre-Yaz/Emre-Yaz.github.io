0662a38e7f5755e714635c073493fdb1ae7039b3
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-09-07 22:27:57 +0300

    A mid commit to see the changes on the website

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+app.use(express.static('FWB'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/FWB/index.html'); 
+});
+
+app.get('/fwb_resume', (req, res) => {
+    res.sendFile(__dirname + '/FWB/fwb_resume.html'); 
+});
+
+app.get('/fwb_contact', (req, res) => {
+    res.sendFile(__dirname + '/FWB/fwb_contact.html'); 
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/FWB/login.html'); 
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/FWB/register.html'); 
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
