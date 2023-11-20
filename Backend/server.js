83f4c4b3dc202d502f1cfc019bd132d6dff1a10b
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-11-20 23:37:25 +0300

    applying the things I learned

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 const app = express();
 const port = 3000;
+const indexRouter = require("./routes/index");
 
-const indexRouter = require('./routes/index');
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('layout', 'layouts/layout');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static(path.join(__dirname, '..', 'docs')));
+app.use(express.static(path.join(__dirname, "..", "docs")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT || port);
