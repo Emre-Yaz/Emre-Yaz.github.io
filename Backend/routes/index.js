21519f80509e2c333fdeac5bd44f3d3e3854c592
Emre-Yaz <ibrahimemreyaz@gmail.com>
2023-11-21 18:21:14 +0300

    helmets on, social icons and favicon added

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,33 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
+const helmet = require("helmet");
+app.use(helmet());
 
-router.get('/test', (req, res) => {
-    res.render(path.join(__dirname, '..', '..', 'views', 'index.ejs')); 
+router.get("/test", (req, res) => {
+  res.render(path.join(__dirname, "..", "..", "views", "index.ejs"));
 });
 
-
-
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'docs', 'index.html')); 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "docs", "index.html"));
 });
 
-router.get('/resume', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'docs', 'resume.html')); 
+router.get("/resume", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "docs", "resume.html"));
 });
 
-router.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'docs', 'contact.html')); 
+router.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "docs", "contact.html"));
 });
 
-router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'docs', 'login.html')); 
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "docs", "login.html"));
 });
 
-router.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'docs', 'register.html')); 
+router.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "docs", "register.html"));
 });
 
-module.exports = router
+module.exports = router;
